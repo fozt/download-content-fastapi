8604ae617a04731b65568981db690b202749e46f
Grigoriy Fedorov <dev.grigoriy@gmail.com>
2022-06-12 22:59:23 +0200

    change openapi_url to env

--- download_content_fastapi/app/config.py
+++ download_content_fastapi/app/config.py
@@ -4,6 +4,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     FILES_URL: str
     FILES_DIR: str
+    OPENAPI_PREFIX: str
 
 
 settings = Settings()
--- download_content_fastapi/main.py
+++ download_content_fastapi/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 from app.endpoints import api_router
 from app.config import settings
 
-app = FastAPI(openapi_url="/openapi.json")
+app = FastAPI(openapi_url=settings.OPENAPI_PREFIX)
 app.include_router(api_router)
 
 if not os.path.exists(settings.FILES_DIR):
